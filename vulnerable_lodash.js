f5c9bd4a2741adfba1fd3c715e295d897c9c3fbf
Madhavan Ramalingam <madhavanr@blackduck.com>
2025-08-21 13:27:19 +0530

    Full Scan Polrish with sca signatur 01

--- vulnerable_lodash.js
+++
@@ -1,9 +0,0 @@
-
-// Vulnerable usage of lodash (version 4.17.11)
-const _ = require('lodash');
-
-// Prototype pollution vulnerability example
-let obj = {};
-_.set(obj, '__proto__.polluted', 'Yes!');
-
-console.log("Polluted?", {}.polluted);
